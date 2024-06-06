039a939dba68e119a3043946c843a574bd7c75c5
Carlos Henrique <chtsca2001@gmail.com>
2024-06-06 11:05:45 -0300

    Criando Migration

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
-from dotenv import load_dotenv
-import os
-
-# Carregar as variáveis do arquivo .env
-load_dotenv()
-
 DEBUG = True
 
-USERNAME = os.getenv('DB_USERNAME', 'default_user')
-PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
-SERVER = os.getenv('DB_SERVER', 'localhost')
-DB = os.getenv('DB_NAME', 'default_db')
+USERNAME = 'root'
+PASSWORD = '1234'
+SERVER = 'localhost'
+DB = 'api_flask'
 
 SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
