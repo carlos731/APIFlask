baa70997ab317125b03b0098d928216f4c672787
Carlos Henrique <chtsca2001@gmail.com>
2024-06-06 12:34:04 -0300

    Criando o Schema de Curso

--- api/__init__.py
+++ api/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 import pymysql
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
 app.config.from_object('config')
-#app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
