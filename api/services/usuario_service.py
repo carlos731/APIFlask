bf13b01a8cc5c90e611ca60398904762d6eb41ca
Carlos Henrique <chtsca2001@gmail.com>
2024-06-12 14:59:45 -0300

    Implementando Login

--- api/services/usuario_service.py
+++ api/services/usuario_service.py
@@ -11,3 +11,6 @@ def cadastrar_usuario(usuario):
     db.session.add(usuario_bd)
     db.session.commit()
     return usuario_bd
+
+def listar_usuario_email(email):
+    return usuario_model.Usuario.query.filter_by(email=email).first()
