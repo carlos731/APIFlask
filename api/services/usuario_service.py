b512887c6f392bb2945b0c8ce42da0ff40174791
Carlos Henrique <chtsca2001@gmail.com>
2024-06-13 17:39:43 -0300

    Regras de Acesso no Access Token

--- api/services/usuario_service.py
+++ api/services/usuario_service.py
@@ -5,7 +5,8 @@ def cadastrar_usuario(usuario):
     usuario_bd = usuario_model.Usuario(
         nome=usuario.nome,
         email=usuario.email,
-        senha=usuario.senha
+        senha=usuario.senha,
+        is_admin=usuario.is_admin
     )
     usuario_bd.encriptar_senha()
     db.session.add(usuario_bd)
@@ -14,3 +15,6 @@ def cadastrar_usuario(usuario):
 
 def listar_usuario_email(email):
     return usuario_model.Usuario.query.filter_by(email=email).first()
+
+def listar_usuario_id(id):
+    return usuario_model.Usuario.query.filter_by(id=id).first()
